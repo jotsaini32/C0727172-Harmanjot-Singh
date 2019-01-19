483d3db75cd297863b215ad73de11809c99474c0
visualstudio <visualstudio@122-PC02>
2019-01-18 21:41:58 -0500

    my final Commit

--- C0727172 Harmanjot Singh/Program.cs
+++ C0727172 Harmanjot Singh/Program.cs
@@ -10,23 +10,48 @@ namespace C0727172_Harmanjot_Singh
     {
         static void Main(string[] args)
         {
-            StudentInformation _Mary = new StudentInformation("Marry", 3.8, 20);
-            StudentInformation _John = new StudentInformation("John", 3.8, 20);
-            StudentInformation _Mark = new StudentInformation("Mark", 3.8, 20);
-            StudentInformation _Steve = new StudentInformation("Steve", 3.8, 20);
-            StudentInformation _Cindy = new StudentInformation("Cindy", 3.8, 20);
+            StudentInformation _Mary = new StudentInformation("Mary", 3.8, 20);
+            StudentInformation _John = new StudentInformation("John", 2.0, 10);
+            StudentInformation _Mark = new StudentInformation("Mark", 0.2, 3);
+            StudentInformation _Steve = new StudentInformation("Steve", 2.9, 16);
+            StudentInformation _Cindy = new StudentInformation("Cindy", 3.9, 187);
+
+            var StudentList = new List<StudentInformation>();
+            StudentList.Add(_Mary);
+            StudentList.Add(_John);
+            StudentList.Add(_Mark);
+            StudentList.Add(_Steve);
+            StudentList.Add(_Cindy);
+
+            foreach (var student in StudentList)
+            {
+                Console.WriteLine(student.Name);
+            }
+
         }
     }
-    class StudentInformation
+
+
+    public class StudentInformation
     {
-        string Name;
-        double GPA;
-        int Attendance;
-        public StudentInformation (string aName, double cGPA, int dAttendance)
+        public string Name;
+        public double GPA;
+        public int Attendance;
+        public StudentInformation(string aName, double cGPA, int cAttendance)
         {
             Name = aName;
             GPA = cGPA;
-            Attendance = dAttendance;
+            Attendance = cAttendance;
+        }
+    }
+
+    class StudentList2
+    {
+        public void CICS()
+        {
+            //var StudentList = new GenericList();
+
+
         }
     }
 }
